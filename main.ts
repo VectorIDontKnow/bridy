7b75347bbc47f3b4b47781fd7321ab43668c33ee
VectorIDontKnow <116850223+VectorIDontKnow@users.noreply.github.com>
2022-10-28 10:04:36 +1100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+let emptyObstacleY = randint(0, 4)
+for (let index = 0; index <= 4; index++) {
+    if (index != emptyObstacleY) {
+        let list: number[] = []
+        list.push(0)
+    }
+}
+basic.forever(function () {
+	
+})
